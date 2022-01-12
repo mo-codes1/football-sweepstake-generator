a7229fc4a650fcc09639eaece885ff4e42aa193d
Mark McLeod <markmcleod45@gmail.com>
2022-01-12 14:34:02 +0000

    Display game page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,14 @@
 class GamesController < ApplicationController
 
-  def index
-  end
-
   def create
     @competition = Competition.find(params[:competition_id])
     @game = @competition.games.create(game_params)
-    # redirect_to game_path(@game.id)
-    # In progress. Not working yet.
+    redirect_to competition_game_url(@competition.id, @game.id)
+  end
+
+  def show
+    @competition = Competition.find(params[:competition_id])
+    @game = Game.find(params[:id])
   end
 
   private
