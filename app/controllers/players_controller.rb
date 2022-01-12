ece2e86ab7fa08274c40946a2b920a03cb1b7414
Mohammed Salah <mohammed_salah@icloud.com>
2022-01-12 14:07:59 +0000

    added new player model

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,4 +4,15 @@ class PlayersController < ApplicationController
 
   def new
   end
+
+  def create
+    @game = Game.find(1) #(replace '1' with :game_id)
+    @player = @game.players.create(player_params)
+  end
+
+  private
+
+  def player_params
+    params.require(:name)
+  end
 end
