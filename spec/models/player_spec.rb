1542954a6c112d1dd82ab1c6a3b6839d12422ba0
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-01-12 14:49:27 +0000

    Add nested form for adding players to a game

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.create' do
+    pending 
+  end 
+  # testing a new instance has been created
+  # testing the database entries?
 end
