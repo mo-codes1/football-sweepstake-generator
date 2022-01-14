433a944b69176f31c8fcc7e5b4e8974cd583a9d8
Mark McLeod <markmcleod45@gmail.com>
2022-01-14 17:26:15 +0000

    Fixed two failing feature tests. Removed two pending tests

--- spec/features/add_player_names_spec.rb
+++ spec/features/add_player_names_spec.rb
@@ -1,23 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Add player names", type: :feature do
+  
   scenario 'A user can add the names of one player in African cup of Nation sweepstake 1' do
-    competition = Competition.create(name: "African cup of Nation")
-    competition.games.create(name: "AFCON")
-    visit "/competitions/1/games/1/players/new"
-    fill_in 'player1', with: 'John'  
+    competition = Competition.create(name: "Africa Cup of Nations")
+    visit "/competitions"
+    click_link "Africa Cup of Nations"
+    fill_in "game[name]", with: "Test sweepstake name"
+    click_button "Create Game"
+    fill_in 'player_player1', with: 'John'  
     click_button 'Generate sweepstake'
     expect(page).to have_content('John')
   end
+
   scenario "a user can add 2 names for the african cup of nations" do
-    competition = Competition.create(name: "African cup of Nation")
-    game = competition.games.create(name: "AFCON")
-    visit "/competitions/2/games/2/players/new"
-    fill_in "player1", with: "john"
-    fill_in "player2", with: "josh"
+    competition = Competition.create(name: "Premier League")
+    visit "/competitions"
+    click_link "Premier League"
+    fill_in "game[name]", with: "Test sweepstake name"
+    click_button "Create Game"
+    fill_in 'player_player1', with: 'John'
+    fill_in 'player_player2', with: 'Dave'  
     click_button 'Generate sweepstake'
-    expect(page).to have_content("john")
-    expect(page).to have_content("josh")
+    expect(page).to have_content('John')
+    expect(page).to have_content('Dave')
   end
 end
 
