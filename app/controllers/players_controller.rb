a778d2686f509d2d0862d9770e0bfcc60ea225ad
Mark McLeod <markmcleod45@gmail.com>
2022-01-14 17:10:57 +0000

    Remove comments and add new comment

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,16 +7,16 @@ class PlayersController < ApplicationController
   end
 
   def new
-    @game = Game.find(params[:game_id]) #(replace '1' with :game_id)
+    @game = Game.find(params[:game_id])
     @player = @game.players.new
   end
 
   def create
     @competition = Competition.find(params[:competition_id])
-    @game = Game.find(params[:game_id]) #(replace '1' with :game_id)
+    @game = Game.find(params[:game_id])
     i = 1
-      24.times do
-        @player = @game.players.create(name: params["player"]["player#{i}"]) # this was an issue
+      24.times do # Replace hardcoded 24 with the number of teams for the competition (@competition.number_of_teams) - Maybe add a new column in competitions table to make this possible
+        @player = @game.players.create(name: params["player"]["player#{i}"])
         i += 1
       end
     redirect_to competition_game_player_url(@competition.id, @game.id, @player.id)
@@ -31,7 +31,7 @@ class PlayersController < ApplicationController
   private
 
   def player_params
-    params.require(:player).permit(:name) ####### there's an issue here.
+    params.require(:player).permit(:name)
   end
   
 end
