5943b748d62a97cc92f837c43d5a49b5dbc35ad8
Mohammed Salah <mohammed_salah@icloud.com>
2022-01-14 12:30:15 +0000

    added 24 players, cant see names on post request

--- spec/features/add_player_names_spec.rb
+++ spec/features/add_player_names_spec.rb
@@ -5,18 +5,16 @@ RSpec.describe "Add player names", type: :feature do
     competition = Competition.create(name: "African cup of Nation")
     competition.games.create(name: "AFCON")
     visit "/competitions/1/games/1/players/new"
-    fill_in 'name', with: 'John'  
+    fill_in 'player1', with: 'John'  
     click_button 'Generate sweepstake'
     expect(page).to have_content('John')
   end
   scenario "a user can add 2 names for the african cup of nations" do
     competition = Competition.create(name: "African cup of Nation")
     game = competition.games.create(name: "AFCON")
-    p competition
-    p game
     visit "/competitions/2/games/2/players/new"
-    fill_in "name", with: "john"
-    fill_in "name", with: "josh"
+    fill_in "player1", with: "john"
+    fill_in "player2", with: "josh"
     click_button 'Generate sweepstake'
     expect(page).to have_content("john")
     expect(page).to have_content("josh")
