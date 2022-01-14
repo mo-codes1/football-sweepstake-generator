7a61ba5770848b1b739b3ecc10e1beb01050431e
olaleye <olaoduola01@gmail.com>
2022-01-14 11:38:08 +0000

    adding another feature test for adding players

--- spec/features/add_player_names_spec.rb
+++ spec/features/add_player_names_spec.rb
@@ -2,11 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Add player names", type: :feature do
   scenario 'A user can add the names of one player in African cup of Nation sweepstake 1' do
-    visit "/players/new"
-    # visit '/competitions/1/sweepstakes/1/players/new'
-    fill_in 'Player 1', with: 'John'  
+    competition = Competition.create(name: "African cup of Nation")
+    competition.games.create(name: "AFCON")
+    visit "/competitions/1/games/1/players/new"
+    fill_in 'name', with: 'John'  
     click_button 'Generate sweepstake'
     expect(page).to have_content('John')
+  end
+  scenario "a user can add 2 names for the african cup of nations" do
+    competition = Competition.create(name: "African cup of Nation")
+    # competition.games.create(name: "AFCON")
+    fill_in "[name]", with: "AFCON"
+    visit "/competitions/1/games/1/players/new"
+    fill_in "player 1", with: "john"
+    fill_in "player 2", with: "josh"
+    click_button 'create game'
+    expect(page).to have_content("john", "josh")
   end
 end
 
