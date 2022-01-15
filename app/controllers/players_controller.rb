65d346f18c9d8e9a915511ab8030cf85a7c32460
Dylan James <djame011@gold.ac.uk>
2022-01-15 11:36:55 +0000

    first commit

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,14 +3,18 @@ class PlayersController < ApplicationController
   def create
     @competition = Competition.find(params[:competition_id])
     @game = Game.find(params[:game_id])
-    @player = @game.players.create(player_params)
-    redirect_to competition_game_player_url(@competition.id, @game.id, @player.id)
+    @teams = Team.all.select { |team| team.competition_id == params[:competition_id].to_i }.count
+    (0...@teams).each do
+      @game.players.create(name: player_params)
+    end
+    p player_params
+    redirect_to competition_game_player_url(@competition.id, @game.id, :id)
   end
 
   def show
     @competition = Competition.find(params[:competition_id])
     @game = Game.find(params[:game_id])
-    @player = Player.find(params[:id])
+    @players = Player.all.select { |player| player.game_id == @game.id }
   end
 
   private
