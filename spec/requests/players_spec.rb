4f5dabd6985e3e683e48a6666c72bf3d1204255a
Mark McLeod <markmcleod45@gmail.com>
2022-01-17 18:18:32 +0000

    Remove test for route no longer used

--- spec/requests/players_spec.rb
+++ spec/requests/players_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Players", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/players/index"
-      expect(response).to have_http_status(:success)
-    end
+
   end
 
 end
