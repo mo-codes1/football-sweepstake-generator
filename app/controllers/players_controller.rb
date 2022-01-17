5bb7eecf6e4bd6b4b22f42f8f437c63914e559e7
Mark McLeod <markmcleod45@gmail.com>
2022-01-17 15:03:14 +0000

    Display player name and non-randomised team for AFCON only

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -15,14 +15,10 @@ class PlayersController < ApplicationController
     @competition = Competition.find(params[:competition_id])
     @game = Game.find(params[:game_id])
     i = 0
-    afcon_arr = [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68]
-    @competition_teams_array = Team.all.select { |team| team.competition_id == @competition.id }
-    @competition_teams_array.map do |team|
-      team.id
-    end
-    p @competition_teams_array
+    @competition_teams_array = Team.all.select { |team| team.competition_id == @competition.id }.map { |team| team.id }
+    p "This is what we're trying to see #{@competition_teams_array}"
       24.times do # Replace hardcoded 24 with the number of teams for the competition (@competition.number_of_teams) - Maybe add a new column in competitions table to make this possible
-        @player = @game.players.create(name: params["player"]["player#{i + 1}"], team_id: afcon_arr[i])
+        @player = @game.players.create(name: params["player"]["player#{i + 1}"], team_id: @competition_teams_array[i])
         i += 1
       end
     redirect_to competition_game_player_url(@competition.id, @game.id, @player.id)
