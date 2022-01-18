ce639c622c77db5c75c1bd9627bb56a9a27aeb04
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-01-18 14:37:55 +0000

    Create Results controller

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,27 @@
+require 'net/http'
+class ResultsController < ApplicationController
+  def index
+    standings = request_standings_api  
+    p "This is standings: #{standings}"
+    
+    standings.inspect
+  end
+
+  private
+  def request_standings_api     
+    url = URI("https://api-football-beta.p.rapidapi.com/standings?season=2021&league=6")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url) 
+    request["x-rapidapi-host"] = 'api-football-beta.p.rapidapi.com'
+    request["x-rapidapi-key"] = '3b09e8b915mshaca1e487c4eda14p11ff9ejsn21ca0d881c0e'
+
+    response = http.request(request)
+    # return nil if response.status != 200
+
+    JSON.parse(response.body)
+  end
+end
