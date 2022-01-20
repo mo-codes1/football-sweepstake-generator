ac53d8086077c153e6c13bd0f2a531d443f5ea17
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-01-20 13:24:19 +0000

    Update database_cleaner and Capybara configs to enable testing of external API call

--- spec/features/user_can_view_team_form_spec.rb
+++ spec/features/user_can_view_team_form_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
-RSpec.feature "View teams current form", type: :feature do
-  scenario "User can click on button to see live form of AFCON" do
+RSpec.feature "View teams current form", type: :feature, js: true do
+
+  scenario "User can click on button to see live form of Premier League", js: true do
+    competition = Competition.create(id: 2, name: "Premier League")
+    team = Team.create(name: "Arsenal", competition_id: competition.id)
+    team2 = Team.create(name: "Brentford", competition_id: competition.id)
+    visit "/competitions"
+    click_link "Create Sweepstake"
+    fill_in "game[name]", with: "Test sweepstake name"
+    click_button "Set up your sweepstake"
+    fill_in 'player_player1', with: 'John'  
+    fill_in 'player_player2', with: 'Dave'  
+    click_button 'Generate sweepstake'
+    click_link 'View real-time results for your sweepstake'
+    expect(page).to have_content("Live standings for your sweepstake:")
+  end
+  
+  scenario "User can click on button to see live form of AFCON", js: true  do
     competition = Competition.create(name: "Africa Cup of Nations")
     team = Team.create(name: "Algeria", competition_id: competition.id)
     team2 = Team.create(name: "Burkina Faso", competition_id: competition.id)
@@ -15,18 +31,6 @@ RSpec.feature "View teams current form", type: :feature do
     click_link 'View real-time results for your sweepstake'
     expect(page).to have_content("Live standings for your sweepstake:")
   end
-  scenario "User can click on button to see live form of Premier League" do
-    competition = Competition.create(name: "Premier League")
-    team = Team.create(name: "Arsenal", competition_id: competition.id)
-    team2 = Team.create(name: "Brentford", competition_id: competition.id)
-    visit "/competitions"
-    click_link "Create Sweepstake"
-    fill_in "game[name]", with: "Test sweepstake name"
-    click_button "Set up your sweepstake"
-    fill_in 'player_player1', with: 'John'  
-    fill_in 'player_player2', with: 'Dave'  
-    click_button 'Generate sweepstake'
-    click_link 'View real-time results for your sweepstake'
-    expect(page).to have_content("Live standings for your sweepstake:")
-  end
+
+  
 end
